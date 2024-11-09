6adfca1b3c9eeef4f096300b62f06876aaa94028
manshusainishab <149303743+manshusainishab@users.noreply.github.com>
2024-11-09 16:50:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "http://localhost:5001";
+export const server = "https://elearning-platform-y9g3.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
